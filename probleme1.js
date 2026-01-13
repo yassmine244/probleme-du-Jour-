e2f52cf6ef2054553af58cbea1f34d63eb2abfb9
yassmine244 <yasminebnr704@gmail.com>
2026-01-13 19:45:07 +0100

    exercice ternime

--- probleme1.js
+++ probleme1.js
@@ -1,5 +1,7 @@
 // 1-Créer une fonction qui combine ton prénom et ton nom et retourne le résultat avec un espace entre les deux.
-
+function nomComplet(prenom, nom) {
+  return prenom + " " + nom;
+}
 
 
 
@@ -9,7 +11,9 @@
 
 
 // 2-Écris une fonction qui prend deux nombres et retourne la moyenne.
-
+function moyenne(a, b) {
+  return (a + b) / 2;
+}
 
 
 
@@ -19,6 +23,9 @@
 
 
 // 3-Crée une fonction qui prend une chaîne de caractères et retourne le nombre de caractères qu'elle contient.
+function nombreDeCaracteres(chaine) {
+  return chaine.length;
+}
 
 
 
@@ -29,6 +36,9 @@
 
 
 // 4-Crée une fonction qui retourne la sous-chaîne à partir d'une position donnée jusqu'à la fin.
+function sousChaine(texte, position) {
+  return texte.substring(position);
+}
 
 
 
